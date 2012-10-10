fca19175965e5abef1c502562349eb18e8fa1db9
Malte Swart <mswart@devtation.de>
2012-10-10 19:36:25 +0200

    api2: improve canteen wrapper (id, __repr__, ids search)
    * basic find methods, currently only mass fetch via ids supported
    * add forgotten id attribute of canteen
    * implement basic __str__, __repr__ method

--- api2.py
+++ api2.py
@@ -5,18 +5,33 @@ from .fields import *
 class Api2Entity(Entity):
 	default_api_base = 'http://openmensa.org/api/v2/'
 
+	def __repr__(self):
+		return self._type + '(' + ','.join(map(lambda v: v + '=' + repr(getattr(self, v)), self._fields)) + ')'
+
 class Canteen(Api2Entity):
+	id = IntegerField()
 	name = StringField()
 	address = StringField()
 	latitude = FloatField()
 	longitude = FloatField()
 
-	def __init__(self, id=None):
+	def __init__(self, id=None, values={}):
 		super(Canteen, self).__init__()
 		if id:
 			self.fromJsonDict(self.request('canteens/{id}'.format(id=int(id))))
 		else:
-			self.fromJsonDict({})
+			self.fromJsonDict(values)
+
+	@staticmethod
+	def find(ids=None):
+		if ids:
+			ids = ','.join(map(lambda i: str(i), ids))
+			cs = Canteen().request('canteens', params = { 'ids': ids })
+			return list(map(lambda c: Canteen(values=c), cs))
+		raise NotImplemented
+
+	def __str__(self):
+		return 'Canteen({id}: {name})'.format(**self.__dict__)
 
 
 class Meal(Api2Entity):
